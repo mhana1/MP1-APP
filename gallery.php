51e9a858bfb9acdcee3efad71e87b2dcb0e74c7a
Mossa Hana <mhana1@hawk.iit.edu>
2015-11-05 17:40:07 -0600

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -74,7 +74,7 @@ $link->real_query($sql);
         if ($result = $link->use_result()) {
             while ($row = $result->fetch_assoc()) {
                 $found = true;
-                echo "<img src =\" " . $row['s3url'] . "\" height='42' width='42' /><img src =\"" .$row['fs3url'] . "\"/>";
+                echo "<img src =\" " . $row['s3url'] . "\" height='200' width='200' /><img src =\"" .$row['fs3url'] . "\"/>";
             }
             $result->close();
         }
