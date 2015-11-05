0a9baeb7b608803d34198d296e10caa0066fc491
Mossa Hana <mhana1@hawk.iit.edu>
2015-11-05 16:45:27 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -35,7 +35,8 @@ $s3->waitUntil('BucketExists',[
 $result = $s3->putObject([
     'ACL' => 'public-read',
     'Bucket' => $bucket,
-   'Key' => $uploadfile
+   'Key' => $bucket,
+   'SourceFile' => $uploadfile
 ]);  
 $url = $result['ObjectURL'];
 echo $url;
@@ -82,6 +83,7 @@ while ($row = $res->fetch_assoc()) {
     echo $row['id'] . " " . $row['uname'] . " " . $row['email']. " " . $row['phone'];
 }
 $link->close();
+header("Location: index.php");
 //add code to detect if subscribed to SNS topic 
 //if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
 // add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
