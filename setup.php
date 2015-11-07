91afd2c7d1e1179603ec4e9ceb06565ea1c302ad
Mossa Hana <mhana1@hawk.iit.edu>
2015-11-06 22:15:32 -0600

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,18 +1,43 @@
 
 <?php
 // Start the session 
-require 'vendor/autoload.php';
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles  -->
+	<link href="css/jumbotron-narrow.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+
+require 'vendor/autoload.php'; 
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
 
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mh-db',
-]);
-// Create a table 
 $result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'mh-db',
 ]);
+
+
+if ($result['DBInstances'][0]['DBInstanceStatus'] != "available"){
+echo '<div class="jumbotron">';
+    echo ' <h2> Wait till the Database is created... </h2></div>';
+
+}
+
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mh-db',]);
+
+
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
 $link = mysqli_connect($endpoint,"controller","letmein888","mhana1DB") or die("Error " . mysqli_error($link)); 
